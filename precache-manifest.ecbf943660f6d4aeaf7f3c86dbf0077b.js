f8b6b1ce72fcd22c9f33f64b5384784eb5bbb70a
Wendi Huang <hwdsjtu97@gmail.com>
2020-06-25 21:40:44 -0700

    Updates

---
+++ precache-manifest.ecbf943660f6d4aeaf7f3c86dbf0077b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86192d89c2de2d33226ccdbcc1751b27",
+    "url": "/nba-web/index.html"
+  },
+  {
+    "revision": "9f929a311a0fe4249adc",
+    "url": "/nba-web/static/css/2.48db8b0f.chunk.css"
+  },
+  {
+    "revision": "7a9b45241f7b2e1bc561",
+    "url": "/nba-web/static/css/main.ee8d5d63.chunk.css"
+  },
+  {
+    "revision": "9f929a311a0fe4249adc",
+    "url": "/nba-web/static/js/2.9f75ced3.chunk.js"
+  },
+  {
+    "revision": "3b03426c8ba9897e5f1adbe1fc390f95",
+    "url": "/nba-web/static/js/2.9f75ced3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a9b45241f7b2e1bc561",
+    "url": "/nba-web/static/js/main.799ff235.chunk.js"
+  },
+  {
+    "revision": "f4f7a9a0fd85066b3641",
+    "url": "/nba-web/static/js/runtime-main.46db1161.js"
+  },
+  {
+    "revision": "9470e2d5c6c7f425edc8e9d8baebdb05",
+    "url": "/nba-web/static/media/nba-logoman-word-white.9470e2d5.svg"
+  }
+]);
